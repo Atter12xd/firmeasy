decef3e5600d1df81e9d6a67bec77d07eb25ec2e
alexanderdiego2001@outlook.com <[tu@email.com](mailto:tu@email.com)>
2024-09-16 18:41:34 -0500

    Refactorizar configuración de Astro para agregar integración con Partytown y actualizar URL del sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://firmeasy.legal",  
   integrations: [react(), partytown()]
 });
